bf9ec26d6db383af415101d8357de798684f966a
pranikc <pranikc@gmail.com>
2017-03-05 18:43:09 -0600

    Updated Sorting Algorithm correctly

--- public/src/home/user-sort.pipeline.js
+++ public/src/home/user-sort.pipeline.js
@@ -15,22 +15,15 @@
         transform: function (list)
         {
             //if hobby is same, sort by name
-            var compareTwoUsersByHobby = function (person1, person2) {
-                return person1.hobby.localeCompare( person2.hobby ); //used of insorting to deterime if it comes before or after
-                                                                     //if string is before, -1, 0 if same, and 1 if after
+            var compareTwoUsers = function (person1, person2) {
+                if (person1.hobby.localeCompare( person2. hobby) === 0)
+                {
+                    return person1.name.localeCompare( person2.name );
+                }
+                return person1.hobby.localeCompare( person2.hobby ); //used in sorting to deterime if it comes before or after//if string is before, -1, 0 if same, and 1 if after
             };
 
-            var compareTwoUsersByName = function(person1, person2) {
-                return person1.name.localeCompare( person2.name );
-            }
-
-            if (compareTwoUsersByHobby() == 0)
-            {
-                return list.slice().sort(compareTwoUsersByName);
-            }
-            else {
-                return list.slice().sort(compareTwoUsersByHobby);
-            }
+            return list.slice().sort(compareTwoUsers);
         }
     });
 }) (window.app || (window.app=={}));
