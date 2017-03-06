b78311feb3bc8f8dd87fd57996cfd6e0bf08b7f2
pranikc <pranikc@gmail.com>
2017-03-05 18:28:29 -0600

    Added Name Sort if Hobby is Same

--- public/src/home/user-sort.pipeline.js
+++ public/src/home/user-sort.pipeline.js
@@ -14,10 +14,23 @@
 
         transform: function (list)
         {
-            var compareTwoUsers = function (person1, person2) {
-                return person1.hobby.localeCompare(person2.hobby);
+            //if hobby is same, sort by name
+            var compareTwoUsersByHobby = function (person1, person2) {
+                return person1.hobby.localeCompare( person2.hobby ); //used of insorting to deterime if it comes before or after
+                                                                     //if string is before, -1, 0 if same, and 1 if after
+            };
+
+            var compareTwoUsersByName = function(person1, person2) {
+                return person1.name.localeCompare( person2.name );
             }
-            return list.slice().sort(compareTwoUsers);
+
+            if (compareTwoUsersByHobby() == 0)
+            {
+                return list.slice().sort(compareTwoUsersByName);
+            }
+            else {
+                return list.slice().sort(compareTwoUsersByHobby);
+            }
         }
     });
 }) (window.app || (window.app=={}));
