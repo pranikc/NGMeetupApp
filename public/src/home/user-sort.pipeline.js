f99f23fa1c76bd3a3acc74917bc738c868aa3890
pranikc <pranikc@gmail.com>
2017-03-05 18:54:58 -0600

    Efficient Sorting Update

--- public/src/home/user-sort.pipeline.js
+++ public/src/home/user-sort.pipeline.js
@@ -16,11 +16,11 @@
         {
             //if hobby is same, sort by name
             var compareTwoUsers = function (person1, person2) {
-                if (person1.hobby.localeCompare( person2. hobby) === 0)
-                {
+                var hobbyCompare = person1.hobby.localeCompare( person2. hobby);
+                if ( hobbyCompare === 0 ){
                     return person1.name.localeCompare( person2.name );
                 }
-                return person1.hobby.localeCompare( person2.hobby ); //used in sorting to deterime if it comes before or after//if string is before, -1, 0 if same, and 1 if after
+                return hobbyCompare;
             };
 
             return list.slice().sort(compareTwoUsers);
